accee922eb30dba772bdcda03fef2ef2b8a732a4
bjvollenhoven <3840572@myuwc.ac.za>
2021-07-02 16:00:28 +0200

    Final beta-diversity questions and workthrough.

--- beta_diversity_01_07_2021.R
+++ beta_diversity_01_07_2021.R
@@ -26,7 +26,7 @@ dim(spp)
 
 #True beta diversity 
 
-true_beta <- data.frame(beta = specnumber(spp, MARGIN = 1) / ncol(spp), # dividing the number of species per row by the total number of columns present.
+true_beta <- data.frame(beta = ncol(spp) / specnumber(spp, MARGIN = 1)  , # dividing the number of species per row by the total number of columns present.
                         #alpha diversity divided by gamma diversity. 
   section_no = c(1:58))
 dim(true_beta)
@@ -53,9 +53,11 @@ ggplot(data = abs_beta, (aes(x = section_no, y = beta))) +
 
 
 # Contemporary definitions of beta diversity ------------------------------
-#library(betapart) this package is needed to perform the follwing functions. 
+#Overall beta diversity (sorensen index) must be decomposed into nestedness-resultant and
+#species turnover in order to understand the processes that underlie beta diversity. 
+#library(betapart) this package is needed to perform the following functions. 
 
-# Decompose total S�rensen dissimilarity into turnover and nestedness-resultant components:
+# Decompose total Sørensen dissimilarity into turnover and nestedness-resultant components:
 Y.core <- betapart.core(spp) #sets up the basic quantities needed to decompose soreness into species turnover and nestedness-resultant 
 Y.pair <- beta.pair(Y.core, index.family = "sor") #this function breaks the data into beta diversity, species turnover and nestedness-resultan
 
@@ -88,5 +90,56 @@ y1_plt <- ggplot(data = y1_plt_data, aes(x = 1:58, y = value)) +
   theme_bw() +
   theme(legend.position = "none")
 y1_plt
+
+#The graph shows increasing dissimilarity between sites as a result of species 
+#turnover. As distance between sites increase so does their dissimilarity. 
+#The sites are relatively close and similarity between sites decrease progressively
+#due to the presence of a linear temperature gradient (Smit et al., 2017). 
+#The species composition change process in taken into account independent
+#of species richness.
+
+
+
+#2. Based on an assessment of literature on the topic, provide a discussion of 
+#nestedness-resultant β-diversity. Use either a marine or terrestrial example to 
+#explain this mode of structuring biodiversity. 
+
+#A high nestedness resultant beta diversity indicates that the mechanism of change 
+#in diversity between sites is based non-gradient ecological processes. These
+#would include ecological factors such as dispersal limitations, or species interactions, 
+#as ass posed to environmental gradients (temperature, salinity). A research paper done by 
+#Baselga (2010) set out how one would deconstruct over all beta diversity in order
+#to properly analyse underlying mechanism driving diversity. In this paper, a clear 
+#difference in mechanisms between nestedness-resultant beta diversity and species turnover
+#was explained. While species turnover was driven by the presence of an environmental gradient,
+#nestedness resultant indicated to a non-gradient influence on diversity. Here, alternative
+#ecological explanations are needed (such as dispersal limitations) to explain changes 
+#in diversity. 
+
+#An example of this difference would be observed with the levels and changes in seaweed diversity along the South African
+#coast. Beta diversity of seaweed species along the South African coast was studies 
+#by Smit et al. in 2017. In the study, high nestedness resultant was observed along 
+#the western coast region as apposed to the high turnover influenced beta diversity 
+#of the east coast. A high species nestedness resultant indicates to a lack of 
+#environmental gradient influence on diversity and favors alternative ecological 
+#influences on diversity.
+
+#Beta diversity would normally be dominated by species turnover but the west coast 
+#is an exception. This can be as consequence of the Benguela current present on the
+#west coast and the Agulhas current on the east. It was suggested that the reasoning 
+#behind the nestedness dominated beta diversity on the west coast was as a result of
+#a possible historic event that established a barrier, limiting connectivity between 
+#the Agulhas and Benguela current. 
+
+#Through this example, one can see how important it is to know the
+#underlying mechanistic effects on beta diversity. In this way, a deeper understanding 
+#of the functionality of beta diversity was highlighted and can be applied and studied
+#in other ecological spaces. 
+
+
+
+
+
+
 
 
